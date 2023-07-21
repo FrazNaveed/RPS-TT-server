1ecb5c90f76462ed238a4f4e43655c89ebfc6d85
fraz <fraz@gmail.com>
2023-07-21 20:43:46 +0500

    FEAT: Added middleware

---
+++ middleware/verifytoken.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const VerifyToken = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied. Token not provided.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ error: 'Invalid token.' });
+  }
+};
+
+module.exports = { VerifyToken };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const controllers = require('./api/index');
+const { VerifyToken } = require('./middleware/verifytoken');
 const { Connect } = require('./utils/dbConnect');
 
 const app = express();
@@ -13,10 +14,10 @@ app.use('/register', controllers.register);
 app.use('/metamask_login', controllers.metamask_login);
 app.use('/email_login', controllers.email_login);
 app.use('/message', controllers.sign_message);
-app.use('/create', controllers.create_car);
-app.use('/update', controllers.update_car);
-app.use('/delete', controllers.delete_car);
-app.use('/read', controllers.read_car);
+app.use('/create', VerifyToken, controllers.create_car);
+app.use('/update', VerifyToken, controllers.update_car);
+app.use('/delete', VerifyToken, controllers.delete_car);
+app.use('/read', VerifyToken, controllers.read_car);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server Started');
